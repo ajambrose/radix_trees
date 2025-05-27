6111c05d63442b690c8d25cf028daeafb3205109
Alex Ambrose <70921165+ajambrose@users.noreply.github.com>
2025-05-26 22:23:02 -0700

    Add entry_ref API, refactor

--- src/entry.rs
+++ src/entry.rs
@@ -1,11 +1,10 @@
-use crate::TrieMap;
 use crate::node::{Link, Node};
-use crate::{KeyMask, TrieKey};
+use crate::{Equivalent, KeyMask, TrieKey, TrieMap};
+use std::borrow::Borrow;
 use std::mem;
 
-pub struct VacantEntry<'a, K: TrieKey, V> {
+pub(super) struct VacantEntryCommon<'a, K: TrieKey, V> {
     tree: &'a mut TrieMap<K, V>,
-    key: K,
     masklen: u32,
     branch_masklen: u32,
     link: Link<K, V>,
@@ -14,11 +13,9 @@ pub struct VacantEntry<'a, K: TrieKey, V> {
     is_right_parent: bool, // for inline insertion, which link does it parent with
 }
 
-impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
-    #[expect(clippy::too_many_arguments)]
-    pub(crate) fn new(
+impl<'a, K: TrieKey, V> VacantEntryCommon<'a, K, V> {
+    pub(super) fn new(
         tree: &'a mut TrieMap<K, V>,
-        key: K,
         masklen: u32,
         branch_masklen: u32,
         link: Link<K, V>,
@@ -26,7 +23,18 @@ impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
         is_right_child: bool,
         is_right_parent: bool,
     ) -> Self {
-        Self { tree, key, masklen, branch_masklen, link, parent, is_right_child, is_right_parent }
+        Self { tree, masklen, branch_masklen, link, parent, is_right_child, is_right_parent }
+    }
+}
+
+pub struct VacantEntry<'a, K: TrieKey, V> {
+    common: VacantEntryCommon<'a, K, V>,
+    key: K,
+}
+
+impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
+    pub(super) fn new(key: K, common: VacantEntryCommon<'a, K, V>) -> Self {
+        Self { common, key }
     }
 
     pub fn key(&self) -> &K {
@@ -47,38 +55,38 @@ impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
         // macro to avoid partial borrow complaints
         macro_rules! add_child_link {
             ($Self:ident, $Link:ident) => {
-                if let Some(p) = $Self.parent.get_mut() {
-                    let c = if $Self.is_right_child { &mut p.right } else { &mut p.left };
+                if let Some(p) = $Self.common.parent.get_mut() {
+                    let c = if $Self.common.is_right_child { &mut p.right } else { &mut p.left };
                     *c = $Link;
                 } else {
-                    assert!($Self.is_right_child); // root node always points "right"
-                    $Self.tree.root = $Link;
+                    assert!($Self.common.is_right_child); // root node always points "right"
+                    $Self.common.tree.root = $Link;
                 }
             };
         }
 
-        if let Some(node) = self.link.get_mut() {
-            assert!(node.masklen >= self.branch_masklen);
-            if node.masklen == self.branch_masklen {
+        if let Some(node) = self.common.link.get_mut() {
+            assert!(node.masklen >= self.common.branch_masklen);
+            if node.masklen == self.common.branch_masklen {
                 // insert data at what used to be a branch node
                 assert!(node.val.is_none());
                 node.val = Some(Box::new((self.key, val)));
-                Self::into_occupied(self.tree, self.link)
-            } else if self.masklen <= self.branch_masklen {
+                Self::into_occupied(self.common.tree, self.common.link)
+            } else if self.common.masklen <= self.common.branch_masklen {
                 // insert the value in-between node and parent, with node as a single child
-                let (left, right) = if self.is_right_parent {
-                    (Link::null(), self.link)
+                let (left, right) = if self.common.is_right_parent {
+                    (Link::null(), self.common.link)
                 } else {
-                    (self.link, Link::null())
+                    (self.common.link, Link::null())
                 };
 
                 let new_node = Box::new(Node {
                     val: Some(Box::new((self.key, val))),
-                    masklen: self.branch_masklen,
+                    masklen: self.common.branch_masklen,
                     left,
                     right,
-                    parent: self.parent,
-                    is_right_child: self.is_right_child,
+                    parent: self.common.parent,
+                    is_right_child: self.common.is_right_child,
                 });
 
                 // can't panic now, start doing all the tree fixup
@@ -86,23 +94,23 @@ impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
                 add_child_link!(self, new_node_link);
                 node.parent = new_node_link;
 
-                Self::into_occupied(self.tree, new_node_link)
+                Self::into_occupied(self.common.tree, new_node_link)
             } else {
                 // create a new branch node with the new node and this existing node as children
                 let new_branch = Box::new(Node {
                     val: None,
-                    masklen: self.branch_masklen,
+                    masklen: self.common.branch_masklen,
                     left: Link::null(),
                     right: Link::null(),
-                    parent: self.parent,
-                    is_right_child: self.is_right_child,
+                    parent: self.common.parent,
+                    is_right_child: self.common.is_right_child,
                 });
 
                 let new_node = Box::new(Node::new_leaf(
                     Some(Box::new((self.key, val))),
-                    self.masklen,
+                    self.common.masklen,
                     Link::null(),
-                    !self.is_right_parent,
+                    !self.common.is_right_parent,
                 ));
 
                 // can't panic now, start doing all the tree fixup
@@ -113,12 +121,12 @@ impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
 
                 add_child_link!(self, new_branch_link);
                 node.parent = new_branch_link;
-                node.is_right_child = self.is_right_parent;
+                node.is_right_child = self.common.is_right_parent;
                 new_node.parent = new_branch_link;
-                let old_link = self.link;
+                let old_link = self.common.link;
 
                 let new_branch = new_branch_link.get_mut().unwrap();
-                let link = if !self.is_right_parent {
+                let link = if !self.common.is_right_parent {
                     new_branch.left = old_link;
                     new_branch.right = new_node_link;
                     new_branch.right
@@ -128,18 +136,18 @@ impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
                     new_branch.left
                 };
 
-                Self::into_occupied(self.tree, link)
+                Self::into_occupied(self.common.tree, link)
             }
         } else {
             // parent has an empty link that we can populate here
             let link = Link::new(Box::new(Node::new_leaf(
                 Some(Box::new((self.key, val))),
-                self.masklen,
-                self.parent,
-                self.is_right_child,
+                self.common.masklen,
+                self.common.parent,
+                self.common.is_right_child,
             )));
             add_child_link!(self, link);
-            Self::into_occupied(self.tree, link)
+            Self::into_occupied(self.common.tree, link)
         }
     }
 
@@ -148,19 +156,53 @@ impl<'a, K: TrieKey, V> VacantEntry<'a, K, V> {
     }
 }
 
+pub struct VacantEntryRef<'a, 'b, K: TrieKey, Q: TrieKey + Equivalent<K>, V> {
+    common: VacantEntryCommon<'a, K, V>,
+    key: &'b Q,
+}
+
+impl<'a, 'b, K: TrieKey, Q: TrieKey + Equivalent<K>, V> VacantEntryRef<'a, 'b, K, Q, V> {
+    pub(super) fn new(key: &'b Q, common: VacantEntryCommon<'a, K, V>) -> Self {
+        Self { common, key }
+    }
+
+    pub fn key(&self) -> &'b Q {
+        self.key
+    }
+
+    pub fn insert(self, val: V) -> &'a mut V
+    where
+        &'b Q: Into<K>,
+    {
+        VacantEntry::from(self).insert(val)
+    }
+
+    pub fn insert_entry(self, val: V) -> OccupiedEntry<'a, K, V>
+    where
+        &'b Q: Into<K>,
+    {
+        VacantEntry::from(self).insert_entry(val)
+    }
+}
+
+impl<'a, 'b, K: TrieKey, Q: TrieKey + Equivalent<K>, V> From<VacantEntryRef<'a, 'b, K, Q, V>>
+    for VacantEntry<'a, K, V>
+where
+    &'b Q: Into<K>,
+{
+    fn from(value: VacantEntryRef<'a, 'b, K, Q, V>) -> Self {
+        Self { key: value.key.into(), common: value.common }
+    }
+}
+
 pub struct OccupiedEntry<'a, K: TrieKey, V> {
     tree: &'a mut TrieMap<K, V>,
     link: Link<K, V>,
-    // _pd: PhantomData<&'a (K, V)>,
 }
 
 impl<'a, K: TrieKey, V> OccupiedEntry<'a, K, V> {
-    pub(crate) fn new(tree: &'a mut TrieMap<K, V>, link: Link<K, V>) -> Self {
-        Self {
-            tree,
-            link,
-            // _pd: PhantomData,
-        }
+    pub(super) fn new(tree: &'a mut TrieMap<K, V>, link: Link<K, V>) -> Self {
+        Self { tree, link }
     }
 
     fn node(&self) -> &'a mut Node<K, V> {
@@ -279,6 +321,87 @@ impl<'a, K: TrieKey, V> Entry<'a, K, V> {
     pub fn or_default(self) -> &'a mut V
     where
         V: Default,
+    {
+        self.or_insert_with(Default::default)
+    }
+}
+
+pub enum EntryRef<'a, 'b, K: TrieKey, Q: TrieKey + Equivalent<K>, V> {
+    Vacant(VacantEntryRef<'a, 'b, K, Q, V>),
+    Occupied(OccupiedEntry<'a, K, V>),
+}
+
+impl<'a, 'b, K: TrieKey, Q: TrieKey + Equivalent<K>, V> EntryRef<'a, 'b, K, Q, V> {
+    pub fn insert(self, val: V) -> OccupiedEntry<'a, K, V>
+    where
+        &'b Q: Into<K>,
+    {
+        match self {
+            EntryRef::Vacant(e) => e.insert_entry(val),
+            EntryRef::Occupied(mut e) => {
+                e.insert_entry(val);
+                e
+            }
+        }
+    }
+
+    pub fn or_insert(self, default: V) -> &'a mut V
+    where
+        &'b Q: Into<K>,
+    {
+        match self {
+            EntryRef::Vacant(e) => e.insert(default),
+            EntryRef::Occupied(e) => e.into_mut(),
+        }
+    }
+
+    pub fn or_insert_with<F: FnOnce() -> V>(self, default: F) -> &'a mut V
+    where
+        &'b Q: Into<K>,
+    {
+        match self {
+            EntryRef::Vacant(e) => e.insert(default()),
+            EntryRef::Occupied(e) => e.into_mut(),
+        }
+    }
+
+    pub fn or_insert_with_key<F: FnOnce(&Q) -> V>(self, default: F) -> &'a mut V
+    where
+        &'b Q: Into<K>,
+    {
+        match self {
+            EntryRef::Vacant(e) => {
+                let val = default(e.key());
+                e.insert(val)
+            }
+            EntryRef::Occupied(e) => e.into_mut(),
+        }
+    }
+
+    pub fn key(&self) -> &Q
+    where
+        K: Borrow<Q>,
+    {
+        match self {
+            EntryRef::Vacant(e) => e.key(),
+            EntryRef::Occupied(e) => e.key().borrow(),
+        }
+    }
+
+    pub fn and_modify<F: FnOnce(&mut V)>(self, f: F) -> Self {
+        match self {
+            EntryRef::Vacant(_) => self,
+            EntryRef::Occupied(mut e) => {
+                f(e.get_mut());
+                EntryRef::Occupied(e)
+            }
+        }
+    }
+
+    pub fn or_default(self) -> &'a mut V
+    where
+        V: Default,
+        &'b Q: Into<K>,
     {
         self.or_insert_with(Default::default)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ mod key_trait;
 mod node;
 mod utils;
 
-use crate::entry::{Entry, OccupiedEntry, VacantEntry};
+use crate::entry::{
+    Entry, EntryRef, OccupiedEntry, VacantEntry, VacantEntryCommon, VacantEntryRef,
+};
 use crate::iter::{Iter, IterMut};
 pub use crate::key_trait::{Equivalent, TrieKey};
 use crate::node::Link;
@@ -18,6 +20,7 @@ pub struct TrieMap<K: TrieKey, V> {
     _pd: PhantomData<(K, V)>,
 }
 
+// Public API
 impl<K: TrieKey, V> TrieMap<K, V> {
     pub fn new() -> Self {
         Self::default()
@@ -37,117 +40,41 @@ impl<K: TrieKey, V> TrieMap<K, V> {
         let _ = crate::iter::IntoIter { curr, len, _pd: PhantomData };
     }
 
-    fn descend<Q: TrieKey + Equivalent<K>>(&self, km: &KeyMask<Q>) -> (Link<K, V>, Link<K, V>) {
-        let mut parent = Link::null();
-        let mut curr = self.root;
-
-        while let Some(node) = curr.get() {
-            if node.masklen >= km.masklen() && node.val.is_some() {
-                break;
-            }
-
-            parent = curr;
-            curr =
-                if branch_bit(km.key().key_bytes(), node.masklen) { node.right } else { node.left };
-        }
-        (curr, parent)
-    }
-
-    fn descend_shortcircuit<Q: TrieKey + Equivalent<K>>(&self, km: &KeyMask<Q>) -> Link<K, V> {
-        let mut curr = self.root;
-
-        while let Some(node) = curr.get() {
-            if node.masklen >= km.masklen() {
-                break;
-            }
-
-            curr =
-                if branch_bit(km.key().key_bytes(), node.masklen) { node.right } else { node.left };
-        }
-        curr
-    }
-
     pub fn entry(&mut self, km: KeyMask<K>) -> Entry<K, V> {
-        let (mut curr, mut parent) = self.descend(&km);
         let (key, masklen) = km.take();
+        self.entry_common(key.key_bytes(), masklen)
+            .map_or_else(|common| Entry::Vacant(VacantEntry::new(key, common)), Entry::Occupied)
+    }
 
-        let (branch_masklen, right) = if let Some(node) = curr.get() {
-            let val = node.val.as_deref().unwrap();
-            let branch_masklen = branch_masklen(val.0.key_bytes(), key.key_bytes());
-
-            if node.masklen == masklen && branch_masklen == masklen {
-                // exact match
-                return Entry::Occupied(OccupiedEntry::new(self, curr));
-            }
-
-            // prepare for backtrack
-            let branch_masklen = std::cmp::min(masklen, branch_masklen);
-            let right = branch_bit(val.0.key_bytes(), branch_masklen);
-            (branch_masklen, right)
-        } else if let Some(p) = parent.get() {
-            // impossible to walk off the tree without parent having a value
-            let val = p.val.as_deref().unwrap();
-            let branch_masklen = branch_masklen(val.0.key_bytes(), key.key_bytes());
-            if branch_masklen >= p.masklen {
-                // simple case - we walked off the tree and should insert a leaf at that spot
-                let is_right_child = branch_bit(key.key_bytes(), p.masklen);
-                return Entry::Vacant(VacantEntry::new(
-                    self,
-                    key,
-                    masklen,
-                    masklen,
-                    curr,
-                    parent,
-                    is_right_child,
-                    false,
-                ));
-            } else {
-                // we walked off the tree but actually the keys differ higher up; prepare for backtrack
-                let right = branch_bit(val.0.key_bytes(), branch_masklen);
-                curr = parent;
-                parent = p.parent;
-                (branch_masklen, right)
-            }
-        } else {
-            return Entry::Vacant(VacantEntry::new(
-                self,
-                key,
-                masklen,
-                masklen,
-                Link::null(),
-                Link::null(),
-                true,
-                false,
-            ));
-        };
-
-        while let Some(p) = parent.get() {
-            // go until we find a node with a lesser mask length
-            if p.masklen < branch_masklen {
-                break;
-            } else {
-                curr = parent;
-                parent = p.parent;
-            }
-        }
-
-        Entry::Vacant(VacantEntry::new(
-            self,
-            key,
-            masklen,
-            branch_masklen,
-            curr,
-            parent,
-            curr.get().unwrap().is_right_child,
-            right,
-        ))
+    pub fn entry_ref<'a, 'b, Q: TrieKey + Equivalent<K>>(
+        &'a mut self,
+        km: KeyMask<&'b Q>,
+    ) -> EntryRef<'a, 'b, K, Q, V> {
+        let (key, masklen) = km.take();
+        self.entry_common(key.key_bytes(), masklen).map_or_else(
+            |common| EntryRef::Vacant(VacantEntryRef::new(key, common)),
+            EntryRef::Occupied,
+        )
     }
 
     pub fn get_exact<Q: TrieKey + Equivalent<K>>(&self, km: KeyMask<Q>) -> Option<&V> {
-        if let Some(node) = self.descend_shortcircuit(&km).get() {
+        if let Some(node) = self.descend_shortcircuit(km.key().key_bytes(), km.masklen()).get() {
             if let Some(val) = node.val.as_deref() {
                 if key_eq(km.key().key_bytes(), km.masklen(), val.0.key_bytes(), node.masklen) {
                     return Some(&val.1);
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn get_exact_mut<Q: TrieKey + Equivalent<K>>(&self, km: KeyMask<Q>) -> Option<&mut V> {
+        if let Some(node) = self.descend_shortcircuit(km.key().key_bytes(), km.masklen()).get_mut()
+        {
+            if let Some(val) = node.val.as_deref_mut() {
+                if key_eq(km.key().key_bytes(), km.masklen(), val.0.key_bytes(), node.masklen) {
+                    return Some(&mut val.1);
                 }
             }
         }
@@ -169,7 +96,7 @@ impl<K: TrieKey, V> TrieMap<K, V> {
     where
         Q: TrieKey + Equivalent<K>,
     {
-        let curr = self.descend_shortcircuit(&km);
+        let curr = self.descend_shortcircuit(km.key().key_bytes(), km.masklen());
         if let Some(node) = curr.get() {
             if let Some(val) = node.val.as_deref() {
                 if key_eq(km.key().key_bytes(), km.masklen(), val.0.key_bytes(), node.masklen) {
@@ -182,12 +109,127 @@ impl<K: TrieKey, V> TrieMap<K, V> {
         None
     }
 
+    // pub fn retain<F: FnMut(KeyMask<&K>, &mut V) -> bool>(&mut self, f: F) {
+    //     todo!()
+    // }
+
+    // pub fn extract_if<F: FnMut(KeyMask<&K>, &mut V) -> bool>(&mut self, f: F) -> ExtractIf<'_, K, V, F> {
+    //     todo!()
+    // }
+
     pub fn iter(&self) -> Iter<K, V> {
         Iter { curr: self.root, len: self.len, _pd: PhantomData }
     }
 
     pub fn iter_mut(&mut self) -> IterMut<K, V> {
         IterMut { curr: self.root, len: self.len, _pd: PhantomData }
+    }
+}
+
+// Private API
+impl<K: TrieKey, V> TrieMap<K, V> {
+    fn entry_common(
+        &mut self,
+        key: &[u8],
+        masklen: u32,
+    ) -> Result<OccupiedEntry<'_, K, V>, VacantEntryCommon<'_, K, V>> {
+        let (mut curr, mut parent) = self.descend(key, masklen);
+
+        let (branch_masklen, right) = if let Some(node) = curr.get() {
+            let val = node.val.as_deref().unwrap();
+            let branch_masklen = branch_masklen(val.0.key_bytes(), key);
+
+            if node.masklen == masklen && branch_masklen == masklen {
+                // exact match
+                return Ok(OccupiedEntry::new(self, curr));
+            }
+
+            // prepare for backtrack
+            let branch_masklen = std::cmp::min(masklen, branch_masklen);
+            let right = branch_bit(val.0.key_bytes(), branch_masklen);
+            (branch_masklen, right)
+        } else if let Some(p) = parent.get() {
+            // impossible to walk off the tree without parent having a value
+            let val = p.val.as_deref().unwrap();
+            let branch_masklen = branch_masklen(val.0.key_bytes(), key);
+            if branch_masklen >= p.masklen {
+                // simple case - we walked off the tree and should insert a leaf at that spot
+                let is_right_child = branch_bit(key, p.masklen);
+                return Err(VacantEntryCommon::new(
+                    self,
+                    masklen,
+                    masklen,
+                    curr,
+                    parent,
+                    is_right_child,
+                    false,
+                ));
+            } else {
+                // we walked off the tree but actually the keys differ higher up; prepare for backtrack
+                let right = branch_bit(val.0.key_bytes(), branch_masklen);
+                curr = parent;
+                parent = p.parent;
+                (branch_masklen, right)
+            }
+        } else {
+            return Err(VacantEntryCommon::new(
+                self,
+                masklen,
+                masklen,
+                Link::null(),
+                Link::null(),
+                true,
+                false,
+            ));
+        };
+
+        while let Some(p) = parent.get() {
+            // go until we find a node with a lesser mask length
+            if p.masklen < branch_masklen {
+                break;
+            } else {
+                curr = parent;
+                parent = p.parent;
+            }
+        }
+
+        Err(VacantEntryCommon::new(
+            self,
+            masklen,
+            branch_masklen,
+            curr,
+            parent,
+            curr.get().unwrap().is_right_child,
+            right,
+        ))
+    }
+
+    fn descend(&self, key: &[u8], masklen: u32) -> (Link<K, V>, Link<K, V>) {
+        let mut parent = Link::null();
+        let mut curr = self.root;
+
+        while let Some(node) = curr.get() {
+            if node.masklen >= masklen && node.val.is_some() {
+                break;
+            }
+
+            parent = curr;
+            curr = if branch_bit(key, node.masklen) { node.right } else { node.left };
+        }
+        (curr, parent)
+    }
+
+    fn descend_shortcircuit(&self, key: &[u8], masklen: u32) -> Link<K, V> {
+        let mut curr = self.root;
+
+        while let Some(node) = curr.get() {
+            if node.masklen >= masklen {
+                break;
+            }
+
+            curr = if branch_bit(key, node.masklen) { node.right } else { node.left };
+        }
+        curr
     }
 }
 
