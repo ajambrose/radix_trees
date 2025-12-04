86f912fbcfed0dd6e2c758bb490e2afc6e34b14c
Alex Ambrose <70921165+ajambrose@users.noreply.github.com>
2025-12-03 17:00:55 -0800

    Add separate tests file for fuzzer-derived tests

--- tests/all/basic.rs
+++ tests/all/basic.rs
@@ -3,20 +3,6 @@ use zerocopy::byteorder::big_endian::U32;
 extern crate std;
 
 #[test]
-fn insert_twice() {
-    let mut t = PTreeMap::new();
-    assert!(t.insert(1, 1).is_none());
-    assert_eq!(t.insert(1, 2), Some(1));
-}
-
-#[test]
-fn insert_masked_twice() {
-    let mut t = PTreeMap::new();
-    assert!(t.insert_exact(KeyMask::new_exact(0u64, 17).unwrap(), 1).is_none());
-    assert_eq!(t.insert_exact(KeyMask::new_exact(0u64, 17).unwrap(), 2), Some(1));
-}
-
-#[test]
 fn default_iter() {
     let t = PTreeMap::<u32, u32>::default();
     let i = t.iter();
@@ -205,24 +191,4 @@ fn iter_suffixes() {
     assert_eq!(t.iter_suffixes("sucralose", true).collect::<Vec<_>>(), vec![]);
     assert_eq!(t.iter_suffixes("superhero", false).collect::<Vec<_>>(), vec![]);
     assert_eq!(t.iter_suffixes("suggestions", true).collect::<Vec<_>>(), vec![]);
-}
-
-#[test]
-fn branch_merge_root() {
-    let keys = [vec![0u8; 0], vec![0; 1], vec![0; 2]];
-    let masks = [0, 6, 12];
-    let vals = 0..3;
-
-    let mut expected: Vec<_> = keys
-        .into_iter()
-        .zip(masks)
-        .map(|(k, m)| KeyMask::new_exact(k, m).unwrap())
-        .zip(vals)
-        .collect();
-
-    let mut t: PTreeMap<_, _> = expected.iter().cloned().collect();
-
-    expected.remove(0);
-    t.remove(vec![]);
-    assert_eq!(expected, t.into_iter().collect::<Vec<_>>());
 }
---
+++ tests/all/from_fuzzer.rs
@@ -0,0 +1,79 @@
+//! Tests derived from failed fuzzer runs.
+
+use radix_trees::ptree::{KeyMask, PTreeMap};
+extern crate std;
+
+#[test]
+fn insert_twice() {
+    let mut t = PTreeMap::new();
+    assert!(t.insert(1, 1).is_none());
+    assert_eq!(t.insert(1, 2), Some(1));
+}
+
+#[test]
+fn insert_masked_twice() {
+    let mut t = PTreeMap::new();
+    assert!(t.insert_exact(KeyMask::new_exact(0u64, 17).unwrap(), 1).is_none());
+    assert_eq!(t.insert_exact(KeyMask::new_exact(0u64, 17).unwrap(), 2), Some(1));
+}
+
+#[test]
+fn branch_merge_root() {
+    let keys = [vec![0u8; 0], vec![0; 1], vec![0; 2]];
+    let masks = [0, 6, 12];
+    let vals = 0..3;
+
+    let mut expected: Vec<_> = keys
+        .into_iter()
+        .zip(masks)
+        .map(|(k, m)| KeyMask::new_exact(k, m).unwrap())
+        .zip(vals)
+        .collect();
+
+    let mut t: PTreeMap<_, _> = expected.iter().cloned().collect();
+
+    expected.remove(0);
+    t.remove(vec![]);
+    assert_eq!(expected, t.into_iter().collect::<Vec<_>>());
+}
+
+#[test]
+fn remove_root() {
+    let keys = [vec![167u8, 167, 167, 16], vec![0; 64], vec![0; 64], vec![0; 41], vec![]];
+    let masks = [32, 254, 255, 253, 0];
+    let vals = 0..5;
+
+    let mut expected: Vec<_> = keys
+        .into_iter()
+        .zip(masks)
+        .map(|(k, m)| KeyMask::new_exact(k, m).unwrap())
+        .zip(vals)
+        .collect();
+
+    let mut t: PTreeMap<_, _> = expected.iter().cloned().collect();
+
+    expected.sort();
+    expected.remove(0);
+    t.remove(vec![]);
+    assert_eq!(expected, t.into_iter().collect::<Vec<_>>());
+}
+
+#[test]
+fn remove_leaf() {
+    let keys = [vec![0u8, 185], vec![255, 28]];
+    let masks = [16, 16];
+    let vals = 0..2;
+
+    let mut expected: Vec<_> = keys
+        .into_iter()
+        .zip(masks)
+        .map(|(k, m)| KeyMask::new_exact(k, m).unwrap())
+        .zip(vals)
+        .collect();
+
+    let mut t: PTreeMap<_, _> = expected.iter().cloned().collect();
+
+    expected.pop();
+    t.remove(vec![255, 28]);
+    assert_eq!(expected, t.into_iter().collect::<Vec<_>>());
+}
